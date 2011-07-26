e4225d1dc62b4e9dacbc654fbc72f0faae3f1a8e
rebecca.dridan <rebecca.dridan@4200e16c-5112-0410-ac55-d7fb557a720a>
2011-07-26 11:50:22 +0000

    short-lived branch for REPP integration

---
+++ branches/repp/cheap/test/tester.cpp
@@ -0,0 +1,66 @@
+/* PET
+ * Platform for Experimentation with efficient HPSG processing Techniques
+ * (C) 2002 Ulrich Callmeier uc@coli.uni-sb.de
+ */
+
+/* main module (unit test driver) */
+
+#include "pet-config.h"
+#include "grammar.h"
+#include "grammar-dump.h"
+#include "settings.h"
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+
+#include <string>
+
+using std::string;
+
+// required global settings from cheap.cpp
+const char * version_string = VERSION ;
+FILE* ferr = stderr;
+FILE* fstatus = stderr;
+FILE* flog = NULL;
+bool XMLServices = false;
+tGrammar *Grammar;
+settings *cheap_settings;
+
+int
+main(int argc, char **argv)
+{
+  // load grammar:
+  if (argc != 2) {
+    printf("usage: %s <grammar-name>\n", argv[0]);
+    return 2;
+  }
+  string grampath = find_file(argv[1], GRAMMAR_EXT);
+  if(grampath.empty()) {
+    fprintf(ferr, "Grammar not found\n");
+    return 3;
+  }
+  string gramname = raw_name(grampath.c_str());
+  cheap_settings = new settings(gramname.c_str(), grampath.c_str(), "reading");
+  Grammar = new tGrammar(grampath.c_str());
+  fprintf(stderr, "\n");
+
+  // create and setup unit testing objects:
+  CppUnit::TestResultCollector result;
+  CppUnit::CompilerOutputter outputter(&result, std::cerr);
+  CppUnit::BriefTestProgressListener progress;
+  CppUnit::TestResult controller;
+  CppUnit::TestRunner runner;
+  controller.addListener(&result);
+  controller.addListener(&progress);
+  runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
+
+  // run tests:
+  runner.run(controller);
+  outputter.write();
+
+  return result.wasSuccessful() ? 0 : 1;
+}
